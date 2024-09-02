4f0e6beec40e36b67a36c1bb480f185d551681fd
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-02 15:17:12 -0400

    added cors config

--- apig-lambda/variables.tf
+++ apig-lambda/variables.tf
@@ -83,7 +83,7 @@ variable "REST_api_authorizer_id" {
 }
 
 variable "enable_cors" {
-  type        = number
+  type        = bool
   description = "Set this to true for, e.g., you have client components (as opposed to server-side components) in your next.js app making API calls to the lambda function"
   default     = false
 }
