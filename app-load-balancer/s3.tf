dffa91ec8166e16833896d64ba5093df74d9ebd6
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-18 14:35:57 -0400

    adding access logging for alb

--- app-load-balancer/s3.tf
+++ app-load-balancer/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "alb_logs" {
   bucket = aws_s3_bucket.alb_logs[count.index].id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
@@ -40,30 +40,32 @@ resource "aws_s3_bucket_lifecycle_configuration" "alb_logs" {
 # Data block to define the policy using aws_iam_policy_document
 data "aws_iam_policy_document" "alb_logs" {
   count = var.enable_logs_to_s3 ? 1 : 0
+  # Allow Elastic Load Balancer to write logs to S3
   statement {
     sid = "AWSLogDeliveryWrite"
     principals {
       type        = "Service"
-      identifiers = ["elasticloadbalancing.amazonaws.com"]
+      identifiers = ["elasticloadbalancing.amazonaws.com"] # ELB service principal
     }
     actions = [
       "s3:PutObject"
     ]
     resources = [
-      "${aws_s3_bucket.alb_logs[count.index].arn}/*"
+      "${aws_s3_bucket.alb_logs[count.index].arn}/*" # Access to bucket objects
     ]
   }
+  # Allow Elastic Load Balancer to check the bucket ACL
   statement {
     sid = "AWSLogDeliveryAclCheck"
     principals {
       type        = "Service"
-      identifiers = ["elasticloadbalancing.amazonaws.com"]
+      identifiers = ["elasticloadbalancing.amazonaws.com"] # ELB service principal
     }
     actions = [
       "s3:GetBucketAcl"
     ]
     resources = [
-      aws_s3_bucket.alb_logs[count.index].arn
+      aws_s3_bucket.alb_logs[count.index].arn # Access to bucket ACL
     ]
   }
 }
