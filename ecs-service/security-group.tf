fa073621f78bf705d5b5f8f04e7135a1ee56c915
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-22 11:01:13 -0400

    added comments

--- ecs-service/security-group.tf
+++ ecs-service/security-group.tf
@@ -15,9 +15,11 @@ resource "aws_security_group_rule" "https_ingress" {
   source_security_group_id = var.source_security_group_id
 }
 
-# rule below is not in gc-res infra, but that ECS task is running fine (this wasn't, task was running, but target group health checks were failing, and task kept getting deregistered from it), not sure why I had to add another sg rule for here but not in gc-res infra...
 # cgpt suggestion:
-# Security Group Configuration: The security group for your ECS service (aws_security_group.ecs_sg) allows inbound traffic on port 80 and 443 from the ALB’s security group. However, since your application is running on port 3000, you should modify the security group rules to allow inbound traffic on port 3000 from the ALB security group.
+# Security Group Configuration: The security group for your ECS service (aws_security_group.ecs_sg) allows 
+# inbound traffic on port 80 and 443 from the ALB’s security group. However, since your application is running 
+# on port 3000, you should modify the security group rules to allow inbound traffic on port 3000 from the 
+# ALB security group.
 resource "aws_security_group_rule" "container_ingress" {
   type                     = "ingress"
   from_port                = var.container_port
@@ -37,3 +39,14 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.self.id
   cidr_blocks       = var.security_group_egress_cidrs
 }
+
+# guide for SGs:
+# https://medium.com/the-cloud-journal/ecs-fargate-with-alb-deployment-using-terraform-part-3-eb52309fdd8f
+
+# commands for when terraform can't delete SGs:
+# aws ec2 delete-security-group --group-id sg-032d87e97958ab62c --profile refayatabid --region ap-south-2
+# aws ec2 describe-network-interfaces --filters Name=group-id,Values=sg-032d87e97958ab62c --query 'NetworkInterfaces[*].NetworkInterfaceId' --output text --profile refayatabid --region ap-south-2
+# aws ec2 delete-network-interface --network-interface-id eni-0b64017b89f8304a3 --profile refayatabid --region ap-south-2
+# aws ec2 describe-network-interfaces --network-interface-ids eni-0b64017b89f8304a3 --profile refayatabid --region ap-south-2
+# aws ec2 detach-network-interface --attachment-id eni-attach-0e8f250bfa3b2b76d --profile refayatabid --region ap-south-2
+# aws ec2 delete-network-interface --network-interface-id eni-0b64017b89f8304a3 --profile refayatabid --region ap-south-2
