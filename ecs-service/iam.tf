c6a18ce2ce782a5ce696fa9298c6c4b2249e07b6
*.tldrlw.com <162769166+tldrlw@users.noreply.github.com>
2024-10-30 14:48:20 -0400

    Update iam.tf

--- ecs-service/iam.tf
+++ ecs-service/iam.tf
@@ -1,3 +1,4 @@
+# Define the IAM role for the ECS task
 resource "aws_iam_role" "ecs_task_execution" {
   name = "${var.app_name}-ecs-task-execution-role"
   assume_role_policy = jsonencode({
@@ -12,6 +13,7 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
+# Policy to allow creating CloudWatch Log Groups
 data "aws_iam_policy_document" "create_log_group_policy_doc" {
   statement {
     actions   = ["logs:CreateLogGroup"]
@@ -26,12 +28,37 @@ resource "aws_iam_policy" "create_log_group_policy" {
   policy      = data.aws_iam_policy_document.create_log_group_policy_doc.json
 }
 
+# Attach CloudWatch policy
 resource "aws_iam_role_policy_attachment" "attach_create_log_group_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = aws_iam_policy.create_log_group_policy.arn
 }
 
+# Attach Amazon ECS Task Execution Role Policy
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# S3 Access Policy Document for All Files in the Bucket
+data "aws_iam_policy_document" "s3_access_policy_doc" {
+  statement {
+    actions   = ["s3:GetObject"]
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::tldrlw-ecs-config-files/*"]  # Access all files in the bucket
+  }
+}
+
+# S3 Access Policy Resource
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "${var.app_name}-s3-access-policy"
+  description = "Policy to allow access to all files in the S3 bucket"
+  policy      = data.aws_iam_policy_document.s3_access_policy_doc.json
+}
+
+# Conditionally Attach S3 Access Policy
+resource "aws_iam_role_policy_attachment" "attach_s3_access_policy" {
+  count      = var.command != null ? 1 : 0  # Attach only if `command` is not null
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
 }
