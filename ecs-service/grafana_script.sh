d328aef7d4dc76f654615a3a1c5ec0d2a5dd7519
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-11-01 15:48:50 -0400

    test

--- ecs-service/grafana_script.sh
+++ ecs-service/grafana_script.sh
@@ -1,4 +1,23 @@
-aws s3 cp s3://tldrlw-ecs-config-files/grafana-datasources.yaml .
-echo File copied
-cd ../../../../
-./run.sh
+#!/bin/bash
+
+# Set variables
+S3_BUCKET="tldrlw-ecs-config-files"
+S3_FILE="grafana-datasources.yaml"
+TARGET_DIR="/etc/grafana/provisioning/datasources"
+
+# Pull the data sources YAML file from S3
+echo "Pulling the data source configuration from S3..."
+aws s3 cp "s3://${S3_BUCKET}/${S3_FILE}" "${TARGET_DIR}/"
+
+if [ $? -eq 0 ]; then
+  echo "File copied successfully to ${TARGET_DIR}"
+else
+  echo "Error: Failed to copy file from S3"
+  exit 1
+fi
+
+# Restart the Grafana service
+echo "Restarting Grafana..."
+/run.sh
+
+# The /run.sh script will start the Grafana service
