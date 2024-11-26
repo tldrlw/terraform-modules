18d4b17c1e7a46758608bf06700e1a92b328d8b2
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-11-26 17:08:22 -0500

    working on apig-lambda-2

--- apig-lambda-2/apig.tf
+++ apig-lambda-2/apig.tf
@@ -1,14 +1,14 @@
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "main" {
   rest_api_id   = var.PRIVATE_APIG_REST_API_ID
   resource_id   = var.PRIVATE_APIG_REST_API_RESOURCE_ID
   http_method   = var.HTTP_METHOD
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_integration" "main" {
   rest_api_id             = var.PRIVATE_APIG_REST_API_ID
   resource_id             = var.PRIVATE_APIG_REST_API_RESOURCE_ID
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.main.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.main.invoke_arn
