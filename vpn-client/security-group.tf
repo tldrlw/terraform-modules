90b4140ad0ddf5f8b14a77c7b423f4c4e6ee75b9
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-11-29 10:24:26 -0500

    vpn-client

--- vpn-client/security-group.tf
+++ vpn-client/security-group.tf
@@ -1,4 +1,3 @@
-# Security Group for Client VPN
 resource "aws_security_group" "client_vpn" {
   name        = "client-vpn-sg"
   description = "Security group for Client VPN"
@@ -14,16 +13,24 @@ resource "aws_vpc_security_group_ingress_rule" "client_vpn_ingress" {
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0" # Replace with specific IP range for better security
+  cidr_ipv4         = "0.0.0.0/0" # Adjust for security
   description       = "Allow HTTPS traffic from VPN clients"
 }
 
 # Egress Rule for Client VPN (Allow all outbound traffic)
 resource "aws_vpc_security_group_egress_rule" "client_vpn_egress" {
   security_group_id = aws_security_group.client_vpn.id
-  # from_port         = 0
-  # to_port           = 0
-  ip_protocol = "-1"        # All protocols
-  cidr_ipv4   = "0.0.0.0/0" # Allow all outbound traffic
-  description = "Allow all outbound traffic"
+  ip_protocol       = "-1"        # All protocols
+  cidr_ipv4         = "0.0.0.0/0" # Allow all outbound traffic
+  description       = "Allow all outbound traffic"
+}
+
+# Ingress Rule for VPN Client-to-VPC Traffic
+resource "aws_vpc_security_group_ingress_rule" "vpn_client_to_vpc" {
+  security_group_id = aws_security_group.client_vpn.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1" # All protocols
+  cidr_ipv4         = var.CLIENT_CIDR_BLOCK
+  description       = "Allow traffic from VPN clients"
 }
