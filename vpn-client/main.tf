25b2e9c153ad9c8025d25887cc88f8382794f6de
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-11-29 11:04:33 -0500

    vpn-client

--- vpn-client/main.tf
+++ vpn-client/main.tf
@@ -116,3 +116,11 @@ resource "aws_acm_certificate_validation" "server_cert_validation" {
   certificate_arn         = aws_acm_certificate.server_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
+
+resource "aws_route53_record" "vpn_dns" {
+  zone_id = var.ROUTE_53_ZONE_ID
+  name    = "vpn.${var.DOMAIN}"
+  type    = "CNAME"
+  records = [aws_ec2_client_vpn_endpoint.main.dns_name]
+  ttl     = 300
+}
